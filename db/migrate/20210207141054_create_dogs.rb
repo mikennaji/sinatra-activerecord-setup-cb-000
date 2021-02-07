27f6deedd8670f90c0961365fa3192bc1df3e8c1
Michael Nnaji <mnnaji3@uwo.ca>
2021-02-07 14:13:37 +0000

    Automatically backed up by Learn

--- db/migrate/20210207141054_create_dogs.rb
+++ db/migrate/20210207141054_create_dogs.rb
@@ -1,8 +1,8 @@
 class CreateDogs < ActiveRecord::Migration[5.2]
   def change
     create_table :dogs do |t|
-      t.string :name 
-      t.string :breed 
-    end 
+      t.string :name
+      t.string :breed
+    end
   end
 end
